f622fff2deeeacffa566d1ed7589626cd6e42c02
Lawlor <steven.d.lawlor@accenture.com>
2017-05-01 11:34:25 +0100

    Commit 5: Creation of functions for read and get commit and first statistical conclusion

--- Analysis.py
+++ Analysis.py
@@ -1,41 +1,51 @@
 # Student number: 10354686
 # CA 4
 
+# Define a function to read in the file and strip into lines
+def read_file(changes_file):
+    # Read in the file and remove any spaces from each of the lines
+    data = [line.strip() for line in open(changes_file, 'r')]
+    return data
 
-# Step 1. Read the file into an object called data
-# Assign a name to the file
-changes_file = 'changes_python.log'
-# Read in the file and remove any spaces from each of the lines
-data = [line.strip() for line in open(changes_file, 'r')]
+# Define a function to get the details related to the strip    
+def get_commits(data):
+    separator = 72*'-'
+    commits = []
+    row_index = 0
+    while row_index < len(data):
+        try:
+            # Find the first line in the file and split out based on the pipe delimeter
+            details = data[row_index + 1].split('|')
+            # Create a dictionary object to assign each of the values
+            commit = {'Revision': details[0].strip(),
+            'Author': details[1].strip(),
+            'Date': details[2].strip().split(' ')[0],
+            'Time': details[2].strip().split(' ')[1],
+            'Number of lines': details[3].strip().split(' ')[0],
+            'Comment': data[row_index+2:data.index('',row_index+1)]}
 
-# Determine the number of lines of the file read in
-print (len(data))
+            # Add the commit object to the commits array
+            commits.append(commit)
+            # Update the index
+            row_index = data.index(separator, row_index + 1) # Search for the separator from the current index plus one
+        except: # This except line is for the index out of range error that will happen at the end of the file. Expected error
+            break
+    return commits
+    
+if __name__ == '__main__':
+    # open the file - and read all of the lines.
+    changes_file = 'changes_python.log'
+    data = read_file(changes_file)
+    commits = get_commits(data)
 
-# Based on visually reviewing the file, we can see that the separator between each of the commits is 72 hyphens in a row
-# Create an opject called separator to define 72 hyphens
-separator = 72*'-'
+# Statistical conclusion #1: There are a total of 422 commits as part of the file
+number_of_commits = str(len(commits))
+print 'There are:',number_of_commits,'commits in the file'
 
-# Let's create an array which will start off as blank
-commits = []
-# Then lets start with a row index of zero which we will use to work through the file
-row_index = 0
+# Statistical conclusion #2:
+print (commits[1]['Author'])
 
-while row_index < len(data):
-    try:
-        # Find the first line in the file and split out based on the pipe delimeter
-        details = data[row_index + 1].split('|')
-        # Create a dictionary object to assign each of the values
-        commit = {'Revision': details[0].strip(),'Author': details[1].strip(),'Date': details[2].strip().split(' ')[0],'Time': details[2].strip().split(' ')[1],'Number of lines': details[3].strip().split(' ')[0]}
 
-        # Add the commit object to the commits array
-        commits.append(commit)
-        # Update the index
-        row_index = data.index(separator, row_index + 1)
-    except: # This except line is for the index out of range error that will happen at the end of the file. Expected error
-        break
-
-# Print the number of commits        
-print len(commits)
 
 
 
