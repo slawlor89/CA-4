e06f95e511763de7d7841277cdd92420c20d4fdc
Lawlor <steven.d.lawlor@accenture.com>
2017-05-02 19:11:46 +0100

    Commit 3: Initial tests based on fixing the analysis file (commit 7)

--- Test_Analysis.py
+++ Test_Analysis.py
@@ -3,5 +3,23 @@
 
 import unittest
 
-from Analysis import *
+from Analysis import read_file, get_commits
 
+class TestCommits(unittest.TestCase):
+	def setUp(self):
+		self.data = read_file('changes_python.log')
+		
+	def test_number_of_lines(self):
+			self.assertEqual(5255, len(self.data))
+		
+	def test_number_of_commits(self):
+		commits = get_commits(self.data)
+		self.assertEqual(422, len(commits))
+
+	def test_first_commit(self):
+		commits = get_commits(self.data)
+		self.assertEqual('Thomas', commits[0]['author'])
+		self.assertEqual('r1551925', commits[0]['revision'])
+
+if __name__ == '__main__':
+    unittest.main()
