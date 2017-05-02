9b308bd27b30ac74cc2be33bc7cac7f186b9f568
Lawlor <steven.d.lawlor@accenture.com>
2017-05-02 20:13:27 +0100

    Commit 5: Test of get_authors and get_dates functions

--- Test_Analysis.py
+++ Test_Analysis.py
@@ -3,7 +3,7 @@
 
 import unittest
 
-from Analysis import read_file, get_commits
+from Analysis import read_file, get_commits, get_authors, get_dates
 
 class TestCommits(unittest.TestCase):
 	def setUp(self):
@@ -19,7 +19,7 @@ class TestCommits(unittest.TestCase):
 	def test_first_commit(self):
 		commits = get_commits(self.data)
 		self.assertEqual('r1551925', commits[0]['revision'])
-		self.assertEqual('Thomas', commits[0]['author']) # Complete this for all elements to be complete
+		self.assertEqual('Thomas', commits[0]['author'])
 		self.assertEqual('2015-11-27', commits[0]['date'])
 		self.assertEqual('16:57:44', commits[0]['time'])
 		self.assertEqual('1', commits[0]['number of lines'])
@@ -29,6 +29,22 @@ class TestCommits(unittest.TestCase):
 		commits = get_commits(self.data)
 		self.assertEqual('Thomas', commits[0]['author'])
 		self.assertEqual('r1551925', commits[0]['revision'])
+		
+	def test_author_count(self):
+		authors = get_authors(self.data)
+		self.assertEqual(191, authors['Thomas'])
+		self.assertEqual(152, authors['Jimmy'])
+		self.assertEqual(1, authors['murari.krishnan'])
+		self.assertEqual(5, authors['Alan'])
+		self.assertEqual(7, authors['Freddie'])
+		self.assertEqual(2, authors['Dave'])
+		self.assertEqual(9, authors['ajon0002'])
+		self.assertEqual(26, authors['Vincent'])
+		
+	def test_date_count(self):
+		dates = get_dates(self.data)
+		self.assertEqual(4, dates['2015-10-16'])
+		
 
 if __name__ == '__main__':
     unittest.main()
